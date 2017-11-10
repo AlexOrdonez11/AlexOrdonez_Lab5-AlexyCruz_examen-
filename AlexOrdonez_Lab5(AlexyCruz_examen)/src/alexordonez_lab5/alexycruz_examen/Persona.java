fec599b4a66b7b8d37b9ae07e3768ed18af047e4
AlexOrdonez11 <ale11davi@gmail.com>
2017-11-10 13:16:26 -0600

    empezando el proyecto

---
+++ AlexOrdonez_Lab5(AlexyCruz_examen)/src/alexordonez_lab5/alexycruz_examen/Persona.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alexordonez_lab5.alexycruz_examen;
+
+/**
+ *
+ * @author Alex Ordonez
+ */
+public class Persona {
+    
+}
